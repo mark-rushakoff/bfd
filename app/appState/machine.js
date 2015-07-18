29b43751169f1055f939f277748c68494a7e1580
Mark Rushakoff <mark.rushakoff@gmail.com>
2015-07-18 15:58:41 -0700

    Implement more bf machine
    Haven't touched decrementing memory or pointer yet.

--- app/appState/machine.js
+++ app/appState/machine.js
@@ -5,8 +5,6 @@ import makeNotifier from '../notifier';
 
 export default function machine() {
   let store = Store({
-    rawCode: '',
-    programCycles: 0,
     instructions() {
       return {
         value: [],
@@ -27,27 +25,92 @@ export default function machine() {
         },
       };
     },
+    instructionPointer: 0,
+    memory: [0],
+    memoryPointer: 0,
+    outputBytes: [],
+    outputString: '',
+    programCycles: 0,
+    programFinished() {
+      return {
+        value: false,
+        deps: {
+          instructions: ['instructions'],
+          instructionPointer: ['instructionPointer'],
+        },
+        get(value, deps) {
+          return deps.instructionPointer >= deps.instructions.length;
+        },
+      };
+    },
+    rawCode: '',
   });
 
   const storeChangedNotifier = makeNotifier();
+  const notifyStoreChanged = storeChangedNotifier.notify;
   return {
     actions: {
       setRawCode(code) {
         store = store.set('rawCode', code);
-        storeChangedNotifier.notify();
+        notifyStoreChanged();
+      },
+      step() {
+        if (store.programFinished) {
+          return;
+        }
+
+        const instruction = store.instructions[store.instructionPointer];
+        switch (instruction) {
+          case bf.INC_PTR:
+            store = store.set('instructionPointer', store.instructionPointer + 1);
+            store = store.set('programCycles', store.programCycles + 1);
+            store = store.set('memoryPointer', store.memoryPointer + 1);
+            if (store.memoryPointer >= store.memory.length) {
+              store = store.memory.push(0);
+            }
+            break;
+          case bf.INC_MEM:
+            store = store.set('instructionPointer', store.instructionPointer + 1);
+            store = store.set('programCycles', store.programCycles + 1);
+            const memPointer = store.memoryPointer;
+            const newMemValue = (store.memory[memPointer] + 1) & 255;
+            store = store.memory.splice(memPointer, 1, newMemValue);
+            break;
+          case bf.PRINT:
+            store = store.set('instructionPointer', store.instructionPointer + 1);
+            store = store.set('programCycles', store.programCycles + 1);
+            const currentValue = store.memory[store.memoryPointer];
+            store = store.outputBytes.push(currentValue);
+            store = store.set('outputString', store.outputString + String.fromCharCode(currentValue));
+            break;
+          default:
+            console.warn('stepped on instruction that is not yet implemented:', instruction); // eslint-disable-line no-console
+        }
+        notifyStoreChanged();
       },
     },
-    getters: {
-      rawCode() {
-        return store.rawCode;
-      },
-      programCycles() {
-        return store.programCycles;
-      },
-      instructions() {
-        return store.instructions;
-      },
-    },
+    getters: exposeGetters([
+      'instructions',
+      'instructionPointer',
+      'memory',
+      'memoryPointer',
+      'outputBytes',
+      'outputString',
+      'programCycles',
+      'programFinished',
+      'rawCode',
+    ]),
     onUpdate: storeChangedNotifier.observe,
   };
+
+  function exposeGetters(getterNames) {
+    const getters = {};
+    getterNames.forEach(getterName => {
+      getters[getterName] = function() {
+        return store[getterName];
+      };
+    });
+
+    return getters;
+  }
 }
--- spec/appstate/machineSpec.js
+++ spec/appstate/machineSpec.js
@@ -3,27 +3,91 @@ import machine from '../../app/appState/machine';
 import makeAppState from '../../app/appState/';
 
 describe('appState.machine', () => {
-  let appState;
+  let actions, getters;
 
   beforeEach(() => {
-    appState = makeAppState();
+    const appState = makeAppState();
     appState.loadPlugin(machine());
+
+    actions = appState.actions;
+    getters = appState.getters;
   });
 
   it('exposes default getters', () => {
-    expect(appState.getters.rawCode()).toEqual('');
-    expect(appState.getters.programCycles()).toEqual(0);
-    expect(appState.getters.instructions()).toEqual([]);
+    expect(getters.rawCode()).toEqual('');
+    expect(getters.programCycles()).toEqual(0);
+    expect(getters.instructions()).toEqual([]);
+    expect(getters.memory()).toEqual([0]);
+    expect(getters.memoryPointer()).toEqual(0);
   });
 
   describe('after updating the raw code', () => {
     beforeEach(() => {
-      appState.actions.setRawCode('>ignored+.');
+      actions.setRawCode('>ignored+.');
     });
 
     it('is reflected in the getters', () => {
-      expect(appState.getters.rawCode()).toEqual('>ignored+.');
-      expect(appState.getters.instructions()).toEqual([bf.INC_PTR, bf.INC_MEM, bf.PRINT]);
+      expect(getters.rawCode()).toEqual('>ignored+.');
+      expect(getters.instructions()).toEqual([bf.INC_PTR, bf.INC_MEM, bf.PRINT]);
+    });
+
+    describe('stepping once (INC_PTR)', () => {
+      beforeEach(() => {
+        actions.step();
+      });
+
+      it('increments the pointer and opens another memory slot', () => {
+        expect(getters.programCycles()).toEqual(1);
+        expect(getters.memory()).toEqual([0, 0]);
+        expect(getters.memoryPointer()).toEqual(1);
+        expect(getters.instructionPointer()).toEqual(1);
+      });
+
+      describe('stepping again (INC_MEM)', () => {
+        beforeEach(() => {
+          actions.step();
+        });
+
+        it('increments the memory', () => {
+          expect(getters.programCycles()).toEqual(2);
+          expect(getters.memory()).toEqual([0, 1]);
+          expect(getters.memoryPointer()).toEqual(1);
+          expect(getters.instructionPointer()).toEqual(2);
+        });
+
+        describe('stepping again (PRINT)', () => {
+          beforeEach(() => {
+            expect(getters.outputBytes()).toEqual([]);
+            expect(getters.outputString()).toEqual('');
+            expect(getters.programFinished()).toBe(false);
+            actions.step();
+          });
+
+          it('adjusts the output', () => {
+            expect(getters.programCycles()).toEqual(3);
+            expect(getters.instructionPointer()).toEqual(3);
+
+            expect(getters.outputBytes()).toEqual([1]);
+            expect(getters.outputString()).toEqual('\x01');
+            expect(getters.programFinished()).toBe(true);
+          });
+
+          describe('attempting to step past the end of the program', () => {
+            beforeEach(() => {
+              actions.step();
+            });
+
+            it('does not change the state', () => {
+              expect(getters.programCycles()).toEqual(3);
+              expect(getters.instructionPointer()).toEqual(3);
+
+              expect(getters.outputBytes()).toEqual([1]);
+              expect(getters.outputString()).toEqual('\x01');
+              expect(getters.programFinished()).toBe(true);
+            });
+          });
+        });
+      });
     });
   });
 });
